4372ea14527ec576282ac092b4a23759ce5b3119
Imriaz <imriaz.cse.bubt@gmail.com>
2023-01-17 17:28:48 +0600

    File read

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,6 +16,7 @@ return array_map(function ($file) {
     public static function find($slug) {
     
     if(! file_exists($path = resource_path("products/{$slug}.html"))){
+        dd($path);
         throw new modelNotFoundException();
     }
     return cache()->remember('products.{$path}', 5, fn ()=> file_get_contents($path));
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
-    <link rel="stylesheet" href="../css/app.css">
-</head>
-<body>
-    <h1>Products List</h1>
+@extends('layout')
+@section('content')
+<?php foreach ($products as $product): ?>
+<div>
+    <?= $product; ?>
+</div>
+<?php endforeach; ?> 
+@endsection
 
-    <?php foreach ($products as $product): ?>
-    <div>
-        <?= $product; ?>
-    </div>
-    <?php endforeach; ?> 
-</body>
-</html>
+    
